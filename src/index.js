d0d25c893f3ccc57f64c5c48ac0d09102e339301
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2018-04-23 02:16:37 -0300

    style: remove index.css and add its config into injectGlobal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { injectGlobal } from 'styled-components'
 
 import { makeMainRoutes } from './routes'
 import store from './reducers'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import './index.css'
+
+injectGlobal`
+  html,
+  body,
+  #root {
+    height: 100%;
+    width: 100vw;
+    overflow-x: hidden;
+  }
+  
+  label, hr {
+    margin: 0;
+  }
+`
 
 const routes = makeMainRoutes()
 
