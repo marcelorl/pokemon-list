b7d013743956ac9ee1728cc3f9769da2c8996766
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2018-04-24 02:26:53 -0300

    feat: slider with images

--- src/components/templates/Pokemon/Pokemon.js
+++ src/components/templates/Pokemon/Pokemon.js
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import get from 'lodash.get'
+import {
+  Col
+} from 'reactstrap'
 
-const Pokemon = () =>
-  <div>asdf</div>
+import Slider from '../../molecules/Slider'
+
+class Pokemon extends Component {
+  createImagesCollection () {
+    const { details, param } = this.props
+
+    return (
+      Object
+        .values(get(details, `list.${param}.images`, []))
+        .filter(item => item !== '')
+        .map(src => ({
+          src,
+          altText: ''
+        }))
+    )
+  }
+
+  render () {
+    const { details, param } = this.props
+
+    if (details.loading) return null
+
+    const images = this.createImagesCollection(details)
+
+    return ([
+      <h2 key='title' className='text-capitalize'>{param}</h2>,
+      <Col key='slider' xs='12' lg='3'>
+        <Slider
+          items={images}
+        />
+      </Col>
+    ])
+  }
+}
 
 export default Pokemon
