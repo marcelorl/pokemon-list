302bd519bf0071bb0326e6cad0cf70cc6e7687bf
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2018-04-25 00:19:18 -0300

    tests: slider

--- src/components/molecules/Slider/Slider.js
+++ src/components/molecules/Slider/Slider.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {
   Carousel,
@@ -81,6 +82,14 @@ class Slider extends Component {
       </CarouselContainer>
     )
   }
+}
+
+Slider.defaultProps = {
+  items: []
+}
+
+Slider.propTypes = {
+  items: PropTypes.array.isRequired
 }
 
 export default Slider
